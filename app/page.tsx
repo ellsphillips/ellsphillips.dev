0703d7e023cda0a1de1614fd6dec72adf23a7a5d
ellsphillips <elliott.phillips.dev@gmail.com>
2024-02-08 17:53:55 +0000

    Feature: define app colour palette

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,12 @@ export default function Home() {
     <>
       <div className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-2">
         <h1 className="font-bold text-2xl">Elliott Phillips</h1>
-        <p className="text-gray-400 font-bold">portfolio built with</p>
+        <p className="text-primary-400 font-bold">
+          made with <span className=" text-secondary-400">&hearts;</span> built
+          with
+        </p>
 
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-cyan-900 after:via-cyan-900 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-primary-600 after:via-primary-300 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-secondary-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-secondary-600 after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#from-sky-900] dark:invert"
             src="/next.svg"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
+const colors = require("tailwindcss/colors")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{ts,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts}",
   ],
   darkMode: "class",
   theme: {
     extend: {
+      spacing: {
+        "9/16": "56.25%",
+      },
+      colors: {
+        primary: colors.violet,
+        secondary: colors.orange,
+        neutral: colors.slate,
+        "placeholder-light": "#F0F0F0",
+        "placeholder-dark": "#252525",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
